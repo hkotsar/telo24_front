bdc75f8c63b7c702d251d43223ecd30c8962ae5d
hkotsar <kotsar.heiki@gmail.com>
2021-11-19 13:35:16 +0200

    telo24

---
+++ main.js
@@ -0,0 +1,23 @@
+const menuBars = document.getElementById('menu-bars');
+const overlay = document.getElementById('overlay');
+const nav1 = document.getElementById('nav-1');
+const nav2 = document.getElementById('nav-2');
+const nav3 = document.getElementById('nav-3');
+
+function toggleNav() {
+    menuBars.classList.toggle('change');
+    overlay.classList.toggle('overlay-active');
+
+    if (overlay.classList.contains('overlay-active')) {
+        overlay.classList.remove('overlay-slide-left');
+        overlay.classList.add('overlay-slide-right');
+    } else {
+        overlay.classList.remove('overlay-slide-right');
+        overlay.classList.add('overlay-slide-left')
+    }
+}
+
+menuBars.addEventListener('click', toggleNav);
+nav1.addEventListener('click', toggleNav);
+nav2.addEventListener('click', toggleNav);
+nav3.addEventListener('click', toggleNav);
